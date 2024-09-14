c259992627d6e39a2151b6713ab8f02ee62e991d
edissonchamorroc <john.chamorro@udea.edu.co>
2024-09-14 17:24:12 -0500

    Se construye calculo de resistencia para cuatro y cinco bandas

---
+++ Laboratorio1/calculadora_resistencias/lib/pages/home_page.dart
@@ -0,0 +1,71 @@
+import 'package:calculadora_resistencias/pages/cinco_bandas_page.dart';
+import 'package:calculadora_resistencias/pages/cuatro_bandas_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calcula Resistencia"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // const Image(
+                //   image: AssetImage('assets/images/notas.png'),
+                //   width: 150,
+                //   height: 150,
+                // ),
+                const Text(
+                  "Cuantas bandas tiene tu resistencia?",
+                  style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const CuatroBandasPage(),
+                      ),
+                    );
+                  },
+                  child: const Text("cuatro bandas"),
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const CincoBandasPage(),
+                      ),
+                    );
+                  },
+                  child: const Text("cinco bandas"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
