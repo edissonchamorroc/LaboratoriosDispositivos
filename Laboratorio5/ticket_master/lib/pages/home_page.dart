15fd0ca9ef20ff01cc0e5f8b95e8086a3840b419
edissonchamorroc <john.chamorro@udea.edu.co>
2024-11-11 17:08:12 -0500

    Se moficia home page con el fin de redirigir el evento seleccionado hacia review page

--- Laboratorio5/ticket_master/lib/pages/home_page.dart
+++ Laboratorio5/ticket_master/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_master/pages/review_page.dart';
 import 'package:ticket_master/respository/ticket_master_api.dart';
 
 class HomePage extends StatefulWidget {
@@ -55,7 +56,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   String getImage(var images) {
-    String ulrImage= '';
+    String ulrImage = '';
     for (var i = 0; i < images.length; i++) {
       if (images[i]['width'] == 305) ulrImage = images[i]['url'];
     }
@@ -65,9 +66,6 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Eventos'),
-      ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -91,23 +89,34 @@ class _HomePageState extends State<HomePage> {
                 itemCount: sportEvents.length,
                 itemBuilder: (context, index) {
                   final event = sportEvents[index];
-                  return Card(
-                    margin: const EdgeInsets.all(10),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Column(
-                      children: [
-                        Image.network(
-                          getImage(event['images']),
-                          fit: BoxFit.fitWidth,
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              ReviewPage(eventId: event['id']),
                         ),
-                        const SizedBox(height: 10),
-                        Text(
-                          event['title'] ?? 'Evento sin título',
-                          style: const TextStyle(fontSize: 15),
-                        ),
-                      ],
+                      );
+                    },
+                    child: Card(
+                      margin: const EdgeInsets.all(10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Column(
+                        children: [
+                          Image.network(
+                            getImage(event['images']),
+                            fit: BoxFit.fitWidth,
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            event['title'] ?? 'Evento sin título',
+                            style: const TextStyle(fontSize: 15),
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 },
@@ -128,23 +137,34 @@ class _HomePageState extends State<HomePage> {
                 itemCount: musicEvents.length,
                 itemBuilder: (context, index) {
                   final event = musicEvents[index];
-                  return Card(
-                    margin: const EdgeInsets.all(10),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Column(
-                      children: [
-                        Image.network(
-                          getImage(event['images']),
-                          fit: BoxFit.fitWidth,
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              ReviewPage(eventId: event['id']),
                         ),
-                        const SizedBox(height: 10),
-                        Text(
-                          event['title'] ?? 'Evento sin título',
-                          style: const TextStyle(fontSize: 15),
-                        ),
-                      ],
+                      );
+                    },
+                    child: Card(
+                      margin: const EdgeInsets.all(10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Column(
+                        children: [
+                          Image.network(
+                            getImage(event['images']),
+                            fit: BoxFit.fitWidth,
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            event['title'] ?? 'Evento sin título',
+                            style: const TextStyle(fontSize: 15),
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 },
